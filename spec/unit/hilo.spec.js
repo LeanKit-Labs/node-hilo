a0a69f50a39173435f705f49998bc2186f9acbbe
Jim Cowart <jim@ifandelse.com>
2015-05-15 01:38:35 -0400

    Adding additional tests to bring coverage to 100%

--- spec/unit/hilo.spec.js
+++ spec/unit/hilo.spec.js
@@ -13,6 +13,9 @@ describe( "node-hilo - unit tests", function() {
 		function getExpected( cnt, startIdx ) {
 			var expected = [];
 			var idx = 0;
+			if(startIdx.equals(0)) {
+				startIdx = startIdx.add(1);
+			}
 			while ( idx < cnt ) {
 				expected.push( startIdx.toString() );
 				startIdx = startIdx.add( 1 );
@@ -21,13 +24,13 @@ describe( "node-hilo - unit tests", function() {
 			return expected;
 		}
 
-		var hivals = [ "1", "10", "100", "1000", "10000", "100000", "1000000", "10000000", "100000000", "1000000000" ].map( function( x ) {
+		var hivals = [ "0", "1", "10", "100", "1000", "10000", "100000", "1000000", "10000000", "100000000", "1000000000" ].map( function( x ) {
 			return bigInt( x, 10 );
 		} );
 
 		hivals.forEach( function( startHival ) {
 			describe( "when hival starts at " + startHival, function() {
-				[ 1, 3, 5, 10, 100 ].forEach( function( maxLo ) {
+				[ 0, 1, 3, 5, 10, 100 ].forEach( function( maxLo ) {
 					describe( "with a maxLo of " + maxLo, function() {
 						var hilo, hival, spy;
 						var idCount = maxLo * 10;
@@ -56,6 +59,69 @@ describe( "node-hilo - unit tests", function() {
 					} );
 				} );
 			} );
+		} );
+	} );
+
+	describe( "when the DB call for next hival fails", function() {
+		describe("with an exception provided", function() {
+			var hilo, hival;
+			beforeEach( function() {
+				var stubiate = {
+					first: function() {
+						return when.reject( new Error( "Databass not OK" ) );
+					},
+					fromFile: function() {}
+				};
+				hilo = getHiloInstance( stubiate, { hilo: { maxLo: 10 } } );
+			} );
+			it( "should reject", function() {
+				return hilo.nextId().should.be.rejectedWith(/Databass not OK/);
+			} );
+		});
+		describe("with no exception provided", function() {
+			var hilo, hival;
+			beforeEach( function() {
+				var stubiate = {
+					first: function() {
+						return when.reject();
+					},
+					fromFile: function() {}
+				};
+				hilo = getHiloInstance( stubiate, { hilo: { maxLo: 10 } } );
+			} );
+			it( "should reject", function() {
+				exception = undefined;
+				return hilo.nextId().should.be.rejectedWith(/An unknown error has occurred/);
+			} );
+		});
+	} );
+
+	describe( "when the DB doesn't return a valid hival", function() {
+		var hilo, hival, spy, dbHival;
+		before( function() {
+			var stubiate = {
+				first: function() {
+					return when( { next_hi: dbHival } );
+				},
+				fromFile: function() {}
+			};
+			spy = sinon.spy( stubiate, "first" );
+			hilo = getHiloInstance( stubiate, { hilo: { maxLo: 10 } } );
+		} );
+
+		it( "should reject if an empty string is returned", function() {
+			dbHival = "";
+			return hilo.nextId().should.be.rejectedWith(/Invalid hival returned from database/);
+		} );
+
+		it( "should reject if an undefined is returned", function() {
+			dbHival = undefined;
+			return hilo.nextId().should.be.rejectedWith(/Invalid hival returned from database/);
+		} );
+
+		it( "should reject if an null is returned", function() {
+			dbHival = null;
+			return hilo.nextId().should.be.rejectedWith(/Invalid hival returned from database/);
 		} );
 	} );
 } );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 var when = require( "when" );
 var machina = require( "machina" );
 var bigInt = require( "big-integer" );
+var util = require( "util" );
+
+function HiloGenerationError() {
+	this.message = util.format.apply( null, arguments );
+	this.name = "HiloGenerationError";
+}
+HiloGenerationError.prototype = Object.create( Error.prototype );
+HiloGenerationError.prototype.constructor = HiloGenerationError;
+
+function transitionToFailure( err ) {
+	this.err = err;
+	this.transition( "dbFailure" );
+}
+
+function processHiValResponse( hv ) {
+	if ( hv.next_hi === null || typeof hv.next_hi === "undefined" || hv.next_hi.length === 0 ) {
+		return transitionToFailure.call( this, new HiloGenerationError( "Invalid hival returned from database: " + hv.next_hi ) );
+	}
+	this.hival = bigInt( hv.next_hi );
+	this.lo = ( this.hival.equals( 0 ) ) ? 1 : 0;
+	this.hi = this.hival.times( this.maxLo + 1 );
+	this.transition( "ready" );
+}
 
 var HiLoFsm = machina.Fsm.extend( {
 
@@ -9,15 +32,16 @@ var HiLoFsm = machina.Fsm.extend( {
 	states: {
 		acquiring: {
 			_onEnter: function() {
-				this.getNextHival().then( function( hv ) {
-					this.hival = bigInt( hv.next_hi );
-					this.lo = ( this.hival === 0 ) ? 1 : 0;
-					this.hi = this.hival.times( this.maxLo + 1 );
-					this.transition( "ready" );
-				}.bind( this ) );
+				this.getNextHival()
+					.then( processHiValResponse.bind( this ), transitionToFailure.bind( this ) );
 			},
 			nextId: function() {
-				this.deferUntilTransition( "ready" );
+				this.deferUntilTransition();
+			}
+		},
+		dbFailure: {
+			nextId: function( done ) {
+				done( this.err || new HiloGenerationError( "An unknown error has occurred." ) );
 			}
 		},
 		ready: {
@@ -47,9 +71,6 @@ module.exports = function( seriate, config ) {
 	var hiloFsm = new HiLoFsm( {
 		initialize: function() {
 			this.maxLo = config.hilo.maxLo;
-			this.hi = undefined;
-			this.lo = undefined;
-			this.hival = undefined;
 			this.getNextHival = function() {
 				return seriate.first( config.sql, {
 					preparedSql: seriate.fromFile( "./sql/nexthi.sql" )
